e48e09704986ff0f7cf27f342124d5d130601a9c
Bernard Zephaniah Addo <92530942+BQuophi@users.noreply.github.com>
2024-01-25 23:34:31 +0000

    Update day2_Input_and_Variables.py

--- 100 days of Python/day2_Input_and_Variables.py
+++ 100 days of Python/day2_Input_and_Variables.py
@@ -1,3 +1,7 @@
+# Day 2 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+# TASK = Build my first interactive code which takes inputs from the user! 
+
+
 myName = input("What's your name?:")
 print("Hi", myName)
 myAge = input("How old are you?:")
