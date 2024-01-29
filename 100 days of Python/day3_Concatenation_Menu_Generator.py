dcb7a89265e72f019f74d0796a931daa2754f9fa
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-01-29 17:45:33 +0000

    Added Day 3 of 100daysofPython

---
+++ 100 days of Python/day3_Concatenation_Menu_Generator.py
@@ -0,0 +1,22 @@
+# Concatenation = Adding texts and variables together
+
+# Example:
+myName = input("Name :")
+print("Hello " , myName)
+whatDay = input("What day is it? :")
+print(myName, "thinks it is", whatDay)
+print()
+# Challenge: Recipe Generator
+food = input("Name a food :")
+plant = input("Name a plant :")
+method = input("Name a method of cooking :")
+burnedfood = input("What word describes burned food? :")
+DIY = input("Name a DIY item :")
+dessert = input("Name a dessert :")
+drink = input("Name a drink :")
+
+print()
+
+print("MENU")
+print(method,food,"with",burnedfood, "on a bed of", DIY,"with a touch of", dessert, "and a glass of", drink)
+
