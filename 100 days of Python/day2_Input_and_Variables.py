23a8746a1ef4f212b0519bb9dc46505401c1c88f
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-01-29 17:10:44 +0000

    Update day2_Input_and_Variables.py

--- 100 days of Python/day2_Input_and_Variables.py
+++ 100 days of Python/day2_Input_and_Variables.py
@@ -2,8 +2,24 @@
 # TASK = Build my first interactive code which takes inputs from the user! 
 
 
-myName = input("What's your name?:")
-print("Hi", myName)
-myAge = input("How old are you?:") 
+# Using the input function together with the print function and a variable
+
+print("Getting to know you!")
+print()
+myName = input("What's your name?: ")
+print("Nice to meet you")
+FavFood = input("Whats your favourite food?: ")
+print("I love that too!")
+FavMusic = input("What's your favourite music?: ")
+print("Great!")
+Live = input("Where do you live?: ")
+print("I've never been there before!")
+print()
+
+print("So you are", myName)
+print("You're probably hungry for", FavFood)
+print("and you're definitely getting your groove on to some", FavMusic,"music")
+print("And you live in", Live)
+print("Wonderful! You rock!")
 
 
