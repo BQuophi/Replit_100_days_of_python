b22a7760886f9d386b8f6759bc9a2b14ef1fb99c
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-01-31 15:37:09 +0000

    Added Day 5

---
+++ 100 days of Python/day5_if_else_Akinator_marvel_character_game.py
@@ -0,0 +1,34 @@
+print("MARVEL MOVIE CHARACTER CREATOR")
+print("------------------------------")
+print()
+print("Answer with lowercase yes or no")
+spiderman = input("Do you like 'hanging around'?: ")
+if spiderman == "yes":
+  print("You're Spider-man!")
+else:
+  print("Then you're not Spider-man")
+
+thor = input("Do you like thunder?: ")
+if thor == "yes":
+  print("Then you're Thor!")
+else:
+  print("Then you're not Thor")
+
+ironman = input("Do you like technology?: ")
+if ironman == "yes":
+  print("Yay! You're Iron-man!")
+else :
+  print("Then you're not Iron-man")
+
+captain = input("Do you feel 'Marvelous'?")
+if captain == "yes":
+  print("Voila, you're Captain America!")
+else:
+  print("Then you're not Captain America")
+
+anyother = input("Are you not any of the above?: ")
+if anyother == "yes":
+  print("Then let's find out who you are!")
+
+newcharacter = input("What is your character?: ")
+print("Hmm, great choice!, Nice to meet you", newcharacter)
