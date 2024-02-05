31efcf568b15841d34372f3a8269577dda98f1b6
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-02-05 21:13:30 +0000

    Added File

---
+++ 100 days of Python/day6_login_proof_system.py
@@ -0,0 +1,14 @@
+print("""
+MY LOGIN SYSTEM
++++++++++++++++""")
+print()
+username = input("Username > ")
+password = input("Password > ")
+if username == "David" and password == "totallyNotBald":
+  print("Why hello there David, what a lovely accent you have, you could have charmed your way in here even without a password")
+elif username == "Kweku" and password == "Kwekuisbald":
+  print("A warm welcome to you Kweku, you are a lovely person")
+elif username == "Lauren" and password == "Laurenisalady":
+  print("Nice to have to here Lauren")
+else : 
+  print("Get outta here pls. System doesn't recognize you")
