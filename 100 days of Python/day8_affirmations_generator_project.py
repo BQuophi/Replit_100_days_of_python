0280496352c14e4c16535d94f09ad164f7e25845
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-02-13 21:40:04 +0000

    Added Day8

---
+++ 100 days of Python/day8_affirmations_generator_project.py
@@ -0,0 +1,18 @@
+print("Wholesome Positivity Machine")
+print("-----------------------------")
+print()
+
+name = input("Who are you? ")
+print("Hi", name)
+day = input("What day is it? ")
+if day == "Monday" or day == "monday" or day == "Tuesday" or day == "tuesday" or "Wednesday" or day == "wednesday" or day == "Thursday" or day == "thursday" or day == "Friday" or day == "friday" or day == "Saturday" or day == "saturday"or day == "Sunday" or day == "sunday":
+  print("Happy", day)
+  achieve = input("What do you want to achieve today? ")
+  print("Nice, that's great!")
+  scale = input("On a scale of 1-10, how do you feel today? ")
+  if scale == "1" or scale == "2" or scale == "3" or scale == "4":
+    print("I can see you're having a tough time, but I'm sure you'll get through it! Keep your chin up!",name,"Today, this",day,"prepare yourself for : ",achieve,"in the most amazing way, simply by being you - YOU ROCK!")
+  if scale == "5" or scale == "6" or scale == "7" or scale == "8" or scale == "9" or scale == "10":
+    print("I'm glad you're having a good day! Keep up the good work",name,". On this",day,"you're going to",achieve,".So trust in the process and you'll achieve it! ❤️✅")
+else :
+  print("That's not a day of a week, Try Again!")
