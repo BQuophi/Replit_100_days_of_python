381857e1e912236852e0e67ada27d20ba52087a3
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-02-13 21:41:59 +0000

    Update day8_affirmations_generator_project.py

--- 100 days of Python/day8_affirmations_generator_project.py
+++ 100 days of Python/day8_affirmations_generator_project.py
@@ -1,5 +1,5 @@
 # Sending out good vibes 😎 with the affirmation generator! 
-#Day 8 of #Replit100DaysOfCode #100DaysOfCode. 
+#D ay 8 of #Replit100DaysOfCode #100DaysOfCode. 
 # Join me on @Replit https://join.replit.com/python
 
 print("Wholesome Positivity Machine")
