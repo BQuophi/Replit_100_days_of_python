13fb629d7d23f2b8e13fffc46b814595ecd6aa5d
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-03-25 09:04:23 +0000

    Update day9_casting_in_python.py

--- 100 days of Python/day9_casting_in_python.py
+++ 100 days of Python/day9_casting_in_python.py
@@ -1,4 +1,5 @@
-Felt like I was at Hogwarts with all the casting I did today 🏰🧙 ! Day 9 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+# Felt like I was at Hogwarts with all the casting I did today 🏰🧙 ! 
+Day 9 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
 
 print("Generation Identifier")
 print("")
