32e1e59488091853f741e1a69a54a846eee76fef
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-03-26 09:23:02 +0000

    Update day_10_Tip_calculator.py

--- 100 days of Python/day_10_Tip_calculator.py
+++ 100 days of Python/day_10_Tip_calculator.py
@@ -1,6 +1,6 @@
 ## Built my own tip calculator! Time to put it to the test at a restaurant 🍕 !
-
-# Day 10 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+# Day 10 of #Replit100DaysOfCode #100DaysOfCode. 
+# Join me on @Replit https://join.replit.com/python
 
 print('TIP CALCULATOR')
 print()
