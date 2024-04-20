5c75b809e487d51957ea38e6c395fbb91ef4f7d7
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-04-20 08:15:20 +0000

    Update day11_seconds_in_a_year.py

--- 100 days of Python/day11_seconds_in_a_year.py
+++ 100 days of Python/day11_seconds_in_a_year.py
@@ -1,3 +1,5 @@
+# How many seconds in a year 🙀 !! Day 11 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+
 print("Project Day : How many seconds are in a year?")
 print()
 
