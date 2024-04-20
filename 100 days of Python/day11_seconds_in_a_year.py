6199afbfdb703a0b2f4850cf8be7bacf42e57610
Bernard Zephaniah Addo-Addo <92530942+BQuophi@users.noreply.github.com>
2024-04-20 08:13:34 +0000

    Added day11

---
+++ 100 days of Python/day11_seconds_in_a_year.py
@@ -0,0 +1,32 @@
+print("Project Day : How many seconds are in a year?")
+print()
+
+print("There are 60 seconds in 1 minute")
+print()
+print("There are 60 minutes in 1 hour")
+print()
+print("There are 24 hours in 1 day")
+print()
+print("There are 365 days in 1 year")
+print()
+
+leap_year = input("Is it a leap year? ")
+if leap_year == "yes" or leap_year == "Yes":
+  print("There are 366 days in a leap year")
+  print()
+  leap_year = 366 * 24 * 60 * 60
+  print("There are", leap_year, "seconds in a leap year")
+  print("Do you also want to know how many seconds there are for non-leap years?")
+  non_leap_year = input("Yes or No? ")
+  if non_leap_year == "yes" or non_leap_year == "Yes":
+    non_leap_year_seconds = 60 * 60 * 24 * 365
+    print("There are", non_leap_year_seconds, "seconds in a year")
+  elif non_leap_year == "no" or non_leap_year == "No":
+    print("Okay, have a nice day!")
+elif leap_year == "no" or leap_year == "No" :
+  print("Then there are 365 days in the year")
+  seconds_in_year = 60 * 60 * 24 * 365
+  print("There are", seconds_in_year, "seconds in a year")
+
+
+  
